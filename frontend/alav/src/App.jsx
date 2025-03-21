5c4f547c651ea08762dcf8bcf2eb303690ab9b59
Baltasar Moreno <baltamoreno444@gmail.com>
2025-03-21 16:19:48 -0300

    prueba

--- frontend/alav/src/App.jsx
+++ frontend/alav/src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/navbar'
 import ListadoMaquinas from "./pages/listadoMaquinas";
 import AgregarMaquina from "./pages/agregarMaquina";
 import DescripcionMaquina from "./pages/descripcionMaquina";
+import ListadoTecnicos from "./pages/listadoTecnicos";
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="/listado-maquina" element={<ListadoMaquinas />} />
         <Route path="/agregar-maquina" element={<AgregarMaquina />} />
         <Route path="/descripcion-maquina" element={<DescripcionMaquina />} />
+        <Route path="/listado-tecnicos" element={<ListadoTecnicos />} />
       </Routes>
     </Router>
   )
